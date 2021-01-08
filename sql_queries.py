6c930faf753c511220ff4c8bbb1c5dada5419fcf
Mekawy5 <mekawy.3f@gmail.com>
2021-01-08 18:15:07 +0200

    add readme.md

--- sql_queries.py
+++ sql_queries.py
@@ -10,6 +10,28 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE_TABLES
 
+# Schema for Song Play Analysis:
+#  a star schema optimized for queries on song play analysis. This includes the following tables.
+
+# Fact Table (songplays) :
+#  - records in log data associated with song plays i.e. records with page NextSong
+# columns :
+# (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+
+# Dimension Tables :
+
+# users - users in the app
+# columns : (user_id, first_name, last_name, gender, level)
+
+# songs - songs in music database
+# columns (song_id, title, artist_id, year, duration)
+
+# artists - artists in music database
+# columns : (artist_id, name, location, latitude, longitude)
+
+# time - timestamps of records in songplays broken down into specific units
+# columns : (start_time, hour, day, week, month, year, weekday)
+
 songplay_table_create = ("""
     CREATE TABLE songplays (
         songplay_id INT PRIMARY KEY,
